4eb37b4a3f2d89f597730f8bfb9d2a13f07a7329
Uriel Ian Gelves <30286558+LeiruG@users.noreply.github.com>
2017-12-08 09:39:40 +0800

    ..!?

--- app/src/main/java/com/gelves/uriel/recipe_list/MainActivity.kt
+++ app/src/main/java/com/gelves/uriel/recipe_list/MainActivity.kt
@@ -15,14 +15,14 @@ class MainActivity : AppCompatActivity() {
         val rv = findViewById<RecyclerView>(R.id.rView)
 
         rv.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
-        val users = ArrayList<UserModel.User>()
+        var users = ArrayList<UserModel.User>()
         users.add(UserModel.User("Curry", "Rustle up a spicy supper using fish, vegetables or meat and a blend of rich flavours."))
         users.add(UserModel.User("Baked Potato", "Perfect your spud technique then pile our filling ideas high. "))
         users.add(UserModel.User("Fish Pie", "Inspiration for a mash-topped seafood bake."))
 
 
 
-        var adapter = CustomAdapter(users)
+        var adapter = CustomAdapter(users,applicationContext)
 
         rv.adapter = adapter
     }

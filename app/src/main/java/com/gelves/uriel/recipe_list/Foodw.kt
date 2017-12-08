08e3dca491b76597ece802dd121a9d3b030bb10f
Uriel Ian Gelves <30286558+LeiruG@users.noreply.github.com>
2017-12-08 08:06:39 +0800

    ..!!

---
+++ app/src/main/java/com/gelves/uriel/recipe_list/Foodw.kt
@@ -0,0 +1,12 @@
+package com.gelves.uriel.recipe_list
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+/**
+ * Created by Lucem on 08/12/2017.
+ */
+class Foodw : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.food_w)
+    }
+}

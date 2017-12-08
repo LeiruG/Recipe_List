d9caa7c5d80873db6f7a72dfd98a0472fdb79aa5
Uriel Ian Gelves <30286558+LeiruG@users.noreply.github.com>
2017-12-08 08:14:12 +0800

    ..!!

--- app/src/main/java/com/gelves/uriel/recipe_list/CustomAdapter.kt
+++ app/src/main/java/com/gelves/uriel/recipe_list/CustomAdapter.kt
@@ -11,125 +11,72 @@ import android.widget.TextView
 /**
  * Created by Lucem on 07/12/2017.
  */
-class CustomAdapter(val userList: ArrayList<UserModel.User>, context: Context) : RecyclerView.Adapter<CustomAdpater.ViewHolder>() {
+class CustomAdapter(val userList: ArrayList<UserModel.User>, context: Context) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
     var ucontext = context
 
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-
         val user : UserModel.User = userList[position]
-
         holder?.textFood?.text = user.food
-
         holder?.textDesc?.text = user.desc
-
         holder!!.textFood.text = "Name: "+ user.food
-
         holder.textDesc.text = "Description: "+user.desc
 
 
-
         holder.onItemClick(object :ItemClick{
-
             override fun onItemClick(view: View, pos: Int) {
-
                 if (pos == 0 ) {
-
-                    val intent = Intent(ucontext, Food1::class.java)
-
+                    val intent = Intent(ucontext, Foodq::class.java)
                     ucontext.startActivity(intent);
-
                 }
 
                 if (pos == 1 ) {
-
-                    val intent = Intent(ucontext, Food2::class.java)
-
+                    val intent = Intent(ucontext, Foodw::class.java)
                     ucontext.startActivity(intent);
-
                 }
 
                 if (pos == 2 ) {
-
-                    val intent = Intent(ucontext, Food3::class.java)
-
+                    val intent = Intent(ucontext, Foode::class.java)
                     ucontext.startActivity(intent);
-
                 }
 
-
             }    })
-
 
     }
 
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
-
         val v = LayoutInflater.from(parent?.context).inflate(R.layout.item_layout, parent,false)
-
         return ViewHolder(v)
-
     }
-
 
 
     override fun getItemCount(): Int {
-
         return userList.size
-
     }
-
-
-
-
-
 
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView), View.OnClickListener {
-
         override fun onClick(v: View?) {
-
             this.uItemClick!!.onItemClick(v!!, adapterPosition)
-
         }
-
-
 
         val textFood: TextView
-
         val textDesc: TextView
-
         val linearLayout: LinearLayout
-
         var uItemClick: ItemClick?=null
 
-
-
         init {
-
             textFood = itemView.findViewById<TextView>(R.id.txtFood)
-
             textDesc = itemView.findViewById<TextView>(R.id.txtDesc)
-
             linearLayout = itemView.findViewById<LinearLayout>(R.id.L_Layout)
-
             itemView.setOnClickListener(this)
-
         }
-
-
 
         fun onItemClick(itemClickListener: ItemClick){
-
             this.uItemClick = itemClickListener
-
         }
-
     }
-
-
-
 
 
 }
